371740b4b187d913ce5d57c71a6a219342f1a9bc
Elitsa <ectky3@gmail.com>
2017-11-13 17:53:10 +0200

    Add files via upload

---
+++ Exam-19March2017/ControlNumber1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlNumber1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int controlNumber = int.Parse(Console.ReadLine());
+
+            int moves = 0;
+            var sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = m; j >=1; j--)
+                {
+                    moves++;
+                    sum += (i * 2 + j * 3);
+                    if (sum>=controlNumber)
+                    {
+                        break;
+                    }
+                }
+                if (sum >= controlNumber)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("{0} moves",moves);
+            if (sum >= controlNumber)
+            {
+                Console.WriteLine("Score: {0} >= {1}",sum,controlNumber);
+            }
+        }
+    }
+}
---
+++ Exam-19March2017/Parallelepiped/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallelepiped
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int firstPoint = 0;
+            int secondPoint = 2 * n;
+
+            Console.WriteLine("+{0}+{1}",new string ('~',n-2),new string('.', 2 * n + 1));
+
+            for (int row = 2; row <= (4*n+4)/2; row++)
+            {
+                Console.WriteLine("|{0}\\{1}\\{2}", new string('.', firstPoint), new string('~', n - 2), new string('.', secondPoint));
+                firstPoint++; secondPoint--;
+            }
+
+            secondPoint = firstPoint - 1; firstPoint = 0;
+            for (int row = (4 * n + 4) / 2; row >= 2; row--)
+            {
+                Console.WriteLine("{0}\\{1}|{2}|", new string('.', firstPoint), new string('.', secondPoint), new string('~', n - 2));
+                firstPoint++; secondPoint--;
+            }
+
+            Console.WriteLine("{0}+{1}+", new string('.', 2 * n + 1),new string('~', n - 2) );
+
+        }
+    }
+}
