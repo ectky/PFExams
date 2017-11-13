0597e88e225d1ce152db296555c1676c486f180f
Elitsa <ectky3@gmail.com>
2017-11-13 18:20:21 +0200

    Add files via upload

---
+++ PFExam-30April2017/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wormtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            length *= 100;
+            double reminderOfDivision = length % width;
+
+            if (reminderOfDivision == 0 || width == 0)
+            {
+                Console.WriteLine($"{width*length:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{length/width*100:F2}%");
+            }
+        }
+    }
+}
