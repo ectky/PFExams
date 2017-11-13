033e6c1e61bc380be441c807835867f02ed8beae
Elitsa <ectky3@gmail.com>
2017-11-13 18:14:08 +0200

    Add files via upload

---
+++ Exam2016/SleepyTom/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepyTom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort freedays = ushort.Parse(Console.ReadLine());
+            var playtime = (365-freedays) * 63 + freedays * 127;
+            var diff = Math.Abs(30000 - playtime);
+            var h = diff / 60;
+            var m = diff % 60;
+            if (playtime>30000)
+                Console.WriteLine("{0} hours and {1} minutes more for play",h,m);
+            else
+                Console.WriteLine("{0} hours and {1} minutes less for play",h,m);
+        }
+    }
+}
---
+++ Exam2016/Transport/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort v = ushort.Parse(Console.ReadLine());
+            ushort p1 = ushort.Parse(Console.ReadLine());
+            ushort p2 = ushort.Parse(Console.ReadLine());
+            float h = float.Parse(Console.ReadLine());
+            var sum = p1*h + p2*h;
+            var per = (sum / v)*100 ;
+            var per1 = 0.0;
+            var per2 = 0.0;
+
+            if (sum <= v)
+            {
+                per1 = (p1*h / sum) * 100;
+                per2 = (p2*h / sum) * 100;
+                Console.WriteLine("The pool is {0}% full. Pipe 1: {1}%. Pipe 2: {2}%.",Math.Floor(per), Math.Floor(per1), Math.Floor(per2));
+            }
+            else
+            {
+                var overfill = sum - v;
+                Console.WriteLine("For {0} hours the pool overflows with {1} liters.",h,overfill);
+            }
+        }
+    }
+}
